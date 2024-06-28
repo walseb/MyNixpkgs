b13db2a9c00f38bc5e0ad0da34a2b562146b46d6
Sebastian Wålinder <s.walinder@gmail.com>
2024-06-29 01:22:14 +0200

    Changes

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -2,10 +2,10 @@
 let
   rev = "98f7ef75d37196e7aaa8ffb848987943221b48a0";
 in
-  (import (fetchTarball {
+  (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     sha256 = "0ha7mxa2bs17xwl9kivx73lmnbmb9fmyw05fx09152bzir2y6p65";
-  }) {})
+  })
 # builtins.fetchGit {
 #   name = "nixpkgs";
 #   url = "https://github.com/nixos/nixpkgs/";
