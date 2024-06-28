ad850b47cacd8ef38bfb9de561a09caaab97a1ff
Sebastian Wålinder <s.walinder@gmail.com>
2024-06-28 16:38:39 +0200

    Generalized more

--- transcribe/whisper-timestamped.nix
+++ transcribe/whisper-timestamped.nix
@@ -1,29 +1,24 @@
 { pkgs, ...}:
-{
-  home.packages = with pkgs; [
-    openai-whisper
-    (python311Packages.buildPythonPackage rec {
-      pname = "whisper-timestamped";
-      version = "master";
-      src = fetchGit {
-        url = "https://github.com/linto-ai/whisper-timestamped";
-        rev = "a82e4d884a504625e8d6a98265272e2cb14c0901";
-        # rev = "a0b86f283336256156f552842d5a99c5101a157a";
-      };
-      doCheck = false;
-      buildInputs = [
-        pkg-s.python3Packages.openai-whisper 
-        pkg-s.python311Packages.dtw-python
-      ];
-      nativeBuildInputs = [
-        pkg-s.python3Packages.openai-whisper 
-        pkg-s.python311Packages.dtw-python
-      ];
-      propagatedBuildInputs = [
-        pkg-s.python3Packages.openai-whisper 
-        pkg-s.python311Packages.dtw-python
-      ];
-    })
+(pkgs.python311Packages.buildPythonPackage rec {
+  pname = "whisper-timestamped";
+  version = "master";
+  src = fetchGit {
+    url = "https://github.com/linto-ai/whisper-timestamped";
+    rev = "a82e4d884a504625e8d6a98265272e2cb14c0901";
+    # rev = "a0b86f283336256156f552842d5a99c5101a157a";
+  };
+  doCheck = false;
+  buildInputs = [
+    pkgs.python3Packages.openai-whisper 
+    pkgs.python311Packages.dtw-python
   ];
-}
-  # https://github.com/linto-ai/whisper-timestamped/commit/a82e4d884a504625e8d6a98265272e2cb14c0901
+  nativeBuildInputs = [
+    pkgs.python3Packages.openai-whisper 
+    pkgs.python311Packages.dtw-python
+  ];
+  propagatedBuildInputs = [
+    pkgs.python3Packages.openai-whisper 
+    pkgs.python311Packages.dtw-python
+  ];
+})
+# https://github.com/linto-ai/whisper-timestamped/commit/a82e4d884a504625e8d6a98265272e2cb14c0901
