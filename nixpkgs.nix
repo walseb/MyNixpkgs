f67a571477761d41beb7dd5441d53a848ed8efed
Sebastian Wålinder <s.walinder@gmail.com>
2024-07-12 23:18:27 +0200

    Simplified nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,8 @@
-{ ... }:
-let
-  rev = "98f7ef75d37196e7aaa8ffb848987943221b48a0";
-in
-  (import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "0ha7mxa2bs17xwl9kivx73lmnbmb9fmyw05fx09152bzir2y6p65";
-  }) {})
+{ rev, sha, ... }:
+(import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  sha256 = sha;
+}) {})
 # builtins.fetchGit {
 #   name = "nixpkgs";
 #   url = "https://github.com/nixos/nixpkgs/";
